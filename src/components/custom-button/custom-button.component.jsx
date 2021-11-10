6a89812fbb5f19f739b643124a903f370279a6ca
Mateo <mtaycruz@gmail.com>
2021-11-10 13:39:11 -0500

    setup redux actions to implement store count

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import './custom-button.style.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProp}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button `}{...otherProp}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProp}) => (
+    <button className={`${inverted ? 'inverted' : '' } ${isGoogleSignIn ? 'google-sign-in' : '' } custom-button `}{...otherProp}>
         {children}
     </button>
 )
